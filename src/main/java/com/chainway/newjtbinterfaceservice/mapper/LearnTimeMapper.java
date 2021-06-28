0081748e2fb0fff5a696a281a956de3f16ceede8
wym <yangming_0404@163.com>
2021-06-28 08:38:53 +0800

    学时备案

--- src/main/java/com/chainway/newjtbinterfaceservice/mapper/LearnTimeMapper.java
+++ src/main/java/com/chainway/newjtbinterfaceservice/mapper/LearnTimeMapper.java
@@ -4,7 +4,16 @@ package com.chainway.newjtbinterfaceservice.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.HashMap;
+
 @Mapper
 public interface LearnTimeMapper {
+
+    /**
+     * @param studyLocusId 
+     * @return
+     */
     boolean LearnTimefunction(@Param("studyLocusId") String studyLocusId);
+
+    HashMap LearnTimeget(@Param("studyLocusId") String studyLocusId);
 }
